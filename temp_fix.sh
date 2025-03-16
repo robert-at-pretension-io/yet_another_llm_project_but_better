0618504081239920efa4be0257ddc9eb67ed8026
Your Name <you@example.com>
2025-03-16 11:56:14 -0400

    changes

--- temp_fix.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i '208s/.*headers modifier should be.*/                  "The headers modifier should match Bearer token for API key");/' tests/executable_blocks_test.rs
--- test.md
+++
@@ -1,4 +0,0 @@
-  <meta:question name="user-query">
-    What insights can be derived from this data?
-  </meta:question>
-
